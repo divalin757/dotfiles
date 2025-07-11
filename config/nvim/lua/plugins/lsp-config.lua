8255c2b6ed65b9ab472f53f3a2ea3bd963b3123e
divalin757 <yosefkesef10@gmail.com>
2025-07-11 13:42:22 +0000

    Update lsp-config.lua

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -18,21 +18,34 @@ return {
     lazy = false,
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
+
+      -- Setup lua_ls with globals and Neovim runtime
       lspconfig.lua_ls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },  -- tell Lua LS that `vim` is OK :contentReference[oaicite:1]{index=1}
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),  -- include Neovim runtime :contentReference[oaicite:2]{index=2}
+              checkThirdParty = false,
+            },
+            runtime = {
+              version = "LuaJIT",
+            },
+            telemetry = { enable = false },
+          },
+        },
       })
 
+      -- Other servers
+      lspconfig.tsserver.setup({ capabilities = capabilities })
+      lspconfig.solargraph.setup({ capabilities = capabilities })
+      lspconfig.html.setup({ capabilities = capabilities })
+
+      -- Common keymaps
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
